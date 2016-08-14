747ea9cbd62ad76cd9ee364c608141f7df38f494
Orkun <orkun1675@hotmail.com>
2016-08-15 00:16:11 +0300

    Add Shield to CatGear
    restore the item in players off hand on death

--- src/main/java/net/steeleyes/catacombs/CatGear.java
+++ src/main/java/net/steeleyes/catacombs/CatGear.java
@@ -13,41 +13,45 @@ import java.util.List;
 public class CatGear {
     private Player player;
 
-    private final List<ItemStack> gear = new ArrayList<>(36);
-    private final ItemStack[] armor = new ItemStack[4];
+    private final ItemStack[] storage;
+    private final ItemStack[] armor;
+    private final ItemStack[] extra;
     private final Location grave;
 
     public CatGear(Player player) {
         this.player = player;
-        PlayerInventory inv = player.getInventory();
         grave = player.getLocation();
+        PlayerInventory inv = player.getInventory();
+        storage = new ItemStack[inv.getStorageContents().length];
+        for (int i = 0; i < storage.length; i++) {
+            ItemStack is = inv.getStorageContents()[i];
+            if (is != null) storage[i] = is.clone();
+        }
+        armor = new ItemStack[inv.getArmorContents().length];
         for (int i = 0; i < armor.length; i++) {
-            ItemStack is = player.getInventory().getArmorContents()[i];
+            ItemStack is = inv.getArmorContents()[i];
             if (is != null) armor[i] = is.clone();
         }
-        for (int i = 0; i <= 35; i++) {
-            ItemStack is = inv.getItem(i);
-            if (is != null) gear.add(is.clone());
+        extra = new ItemStack[inv.getExtraContents().length];
+        for (int i = 0; i < extra.length; i++) {
+            ItemStack is = inv.getExtraContents()[i];
+            if (is != null) extra[i] = is.clone();
         }
     }
 
     public void dropGear() {
         World world = grave.getWorld();
+        for (ItemStack is : storage) if (is != null && is.getType() != Material.AIR) world.dropItem(grave, is);
         for (ItemStack is : armor) if (is != null && is.getType() != Material.AIR) world.dropItem(grave, is);
-        for (ItemStack is : gear) if (is != null && is.getType() != Material.AIR) world.dropItem(grave, is);
+        for (ItemStack is : extra) if (is != null && is.getType() != Material.AIR) world.dropItem(grave, is);
     }
 
     public void restoreGear() {
         PlayerInventory inv = player.getInventory();
-        ItemStack[] currentArmor = inv.getArmorContents();
-        for (int i = 0; i < armor.length; i++)
-            if (armor[i] != null && armor[i].getType() != Material.AIR) currentArmor[i] = armor[i];
-        inv.setArmorContents(currentArmor);
+        inv.setStorageContents(storage);
+        inv.setArmorContents(armor);
+        inv.setExtraContents(extra);
         player.updateInventory();
-        for (int i = 0; i < gear.size(); i++) {
-            ItemStack stk = gear.get(i);
-            if (stk != null && stk.getType() != Material.AIR) inv.setItem(i, gear.get(i));
-        }
     }
 
 }
